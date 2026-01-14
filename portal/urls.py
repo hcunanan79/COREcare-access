292834ada3483e1944e068b7136acf037c2f951f
Hazel Cunanan <ccareaccess@gmail.com>
2026-01-13 18:11:49 -0800

    Add dashboard route

--- portal/urls.py
+++ portal/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.portal_home, name="portal_home"),
     path("signup/", views.signup, name="signup"),
+    path("dashboard/", views.employee_dashboard, name="employee_dashboard"),
 ]
-
