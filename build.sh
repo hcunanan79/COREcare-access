70f80eeb2c5968300f95c4c211bd2057d3ac743f
Hazel Cunanan <hcunanan79@gmail.com>
2026-01-16 15:18:24 -0800

    docs: clarify build script steps for production deployment

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
-# Run database migrations
+# Run database migrations (apply all pending schema changes)
 python manage.py migrate
 
-# Collect static files
+# Collect static files for production serving
 python manage.py collectstatic --noinput
